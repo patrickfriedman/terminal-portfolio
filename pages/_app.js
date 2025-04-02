114d5392906b684f375886fce1d864103f2df091
patrickfriedman <patrick.s.friedman@gmail.com>
2025-04-01 19:46:24 -0500

    test 3

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,10 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        <Component {...pageProps} />
-        <Analytics />
-      </body>
-    </html>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
 
